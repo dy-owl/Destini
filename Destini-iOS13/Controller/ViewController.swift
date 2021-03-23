63f54e67b3cd4edac6548414053b3362c50ef6bc
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-03-23 21:28:29 +0200

    Created updateUI() function for changing Story text according to user's choice.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,21 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    
-    
     let stories = [
         Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
         Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
         Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
     ]
     
+    var storyNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = stories[0].title
-        choice1Button.setTitle(stories[0].choice1, for: .normal)
-        choice2Button.setTitle(stories[0].choice2, for: .normal)
+        updateUI()
     }
     
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.currentTitle == stories[storyNumber].choice1 {
+            storyNumber += 1
+            updateUI()
+        } else if sender.currentTitle == stories[storyNumber].choice2 {
+            storyNumber += 2
+            updateUI()
+        }
+    }
     
+    func updateUI() {
+        storyLabel.text = stories[storyNumber].title
+        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
+    }
 }
-
